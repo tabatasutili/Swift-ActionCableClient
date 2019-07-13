6a5714e2bad7be6afdac635def481e93d129dd8a
rogeriowiethornjr <36937774+rogeriowiethornjr@users.noreply.github.com>
2019-07-13 14:54:35 -0300

    Update Channel.swift

--- Source/Classes/Channel.swift
+++ Source/Classes/Channel.swift
@@ -50,7 +50,7 @@ open class Channel: Hashable, Equatable {
     open var uid: String {
         get {
             //defaults to channel name
-            var channelUID = name
+            let channelUID = name
             
             //if identifier isn't empty, fetch the first value as the channel unique identifier
             //if let dictionary = identifier?.first {
